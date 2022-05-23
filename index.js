f3fdec43300b00838f1e9c1401b5a446ed015b05
mariah622 <mjustine622@gmail.com>
2022-05-23 18:39:54 -0400

    added features

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ let h3 = document.querySelector('h3');
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let body  = document.getElementById('gradient')
+let randomButton = document.getElementById('random')
+
+const rgbToHex = rgb => '#' + (rgb.match(/[0-9|.]+/g).map((x, i) => i === 3 ? parseInt(255 * parseFloat(x)).toString(16) : parseInt(x).toString(16)).join('')).padStart(2, '0').toUpperCase();
+
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+randomButton.addEventListener('click', setRandomGradient);
 
 function setGradient(){
     body.style.background ='linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')'; 
     
     h3.innerText = body.style.background + ";"
 
+}
+
+function setRandomGradient(){
+    console.log('finding random color....')
+    let col1 = "rgb" + "(" + (Math.floor(Math.random() * 255) +
+    ", " +
+    Math.floor(Math.random() * 255) +
+    ", " +
+    Math.floor(Math.random() * 255) +
+    ")" + ";");
+
+    let col2 = "rgb" + "(" + (Math.floor(Math.random() * 255) +
+    ", " +
+    Math.floor(Math.random() * 255) +
+    ", " +
+    Math.floor(Math.random() * 255) +
+    ")" + ";");
+
+    color1.value=rgbToHex(col1)
+    color2.value=rgbToHex(col2)
+
+    setGradient();
 }
